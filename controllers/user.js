799c0b03f5d2670a5d3a7cb745259a3a09a1483e
hoangvinh13159200 <hoangvinh13159200@gmail.com>
2021-06-01 17:05:13 +0700

    1/6

--- controllers/user.js
+++ controllers/user.js
@@ -5,99 +5,76 @@ const Order = require("../models/order");
 const { json } = require("express");
 
 
-exports.getAdmin = (req, res, next)=>{
-  var cartProduct;
-  if (!req.session.cart) {
-    cartProduct = null;
-  } else {
-    var cart = new Cart(req.session.cart);
-    cartProduct = cart.generateArray();
-  }
-  // Users.find().then(user =>{
-  //   Products.find().then(products => {
-  //     Order.find().then((order) => {
-  //       console.log(order[1].cart.totalPrice)
-  //       console.log("So luong user la:")
-  //       console.log(user.lenght)
-  //       console.log(product.lenght)
-  //       res.render("admin", {
-  //         title: "Admin",
-  //         // user: req.user,
-  //         // cartProduct: cartProduct,
-  //         order: order,
-  //         user: user ,
-  //         proc : products,
-  //         // messageSucc: messageSucc,
-  //         // messageError:messageError
-  //       });
-  //     });
-  //   })
-  // })
-  var users = Users.find()
-  var products = Products.find()
-  const messageSucc = req.flash("success")[0];
-  const messageError = req.flash("error")[0];
-  Order.find().then((order) => {
-    console.log(order[1].cart.totalPrice)
-    // console.log("So luong user la:")
-    // console.log(user.lenght)
-    // console.log(product.lenght)
-    res.render("admin", {
-      title: "Admin",
-      // user: req.user,
-      // cartProduct: cartProduct,
-      order: order,
-      user: users ,
-      proc : products,
-      // messageSucc: messageSucc,
-      // messageError:messageError
+exports.getAdmin = (req, res, next) => {
+    var cartProduct;
+    if (!req.session.cart) {
+        cartProduct = null;
+    } else {
+        var cart = new Cart(req.session.cart);
+        cartProduct = cart.generateArray();
+    }
+    var users = Users.find()
+    var products = Products.find()
+    console.log(users)
+    const messageSucc = req.flash("success")[0];
+    const messageError = req.flash("error")[0];
+    Order.find().then((order) => {
+        res.render("admin", {
+            title: "Admin",
+            // user: req.user,
+            // cartProduct: cartProduct,
+            order: order,
+            user: users,
+            proc: products,
+            // messageSucc: messageSucc,
+            // messageError:messageError
+        });
     });
-  });
 }
 
 exports.getAccount = (req, res, next) => {
-  var cartProduct;
-  if (!req.session.cart) {
-    cartProduct = null;
-  } else {
-    var cart = new Cart(req.session.cart);
-    cartProduct = cart.generateArray();
-  }
-  const messageSucc = req.flash("success")[0];
-  const messageError = req.flash("error")[0];
-  Order.find({ user: req.user }).then(order => {
-    res.render("account", {
-      title: "Thông tin tài khoản",
-      user: req.user,
-      cartProduct: cartProduct,
-      order: order,
-      messageSucc: messageSucc,
-      messageError:messageError
+    var cartProduct;
+    if (!req.session.cart) {
+        cartProduct = null;
+    } else {
+        var cart = new Cart(req.session.cart);
+        cartProduct = cart.generateArray();
+    }
+    const messageSucc = req.flash("success")[0];
+    const messageError = req.flash("error")[0];
+    Order.find({ user: req.user }).then(order => {
+        res.render("account", {
+            title: "Thông tin tài khoản",
+            user: req.user,
+            cartProduct: cartProduct,
+            order: order,
+            messageSucc: messageSucc,
+            messageError: messageError
+        });
     });
-  });
 };
 
 exports.getAccountChange = (req, res, next) => {
-  var cartProduct;
-  if (!req.session.cart) {
-    cartProduct = null;
-  } else {
-    var cart = new Cart(req.session.cart);
-    cartProduct = cart.generateArray();
-  }
-  res.render("account-change-info", {
-    title: "Thay đổi thông tin tài khoản",
-    user: req.user,
-    cartProduct: cartProduct
-  });
+    var cartProduct;
+    if (!req.session.cart) {
+        cartProduct = null;
+    } else {
+        var cart = new Cart(req.session.cart);
+        cartProduct = cart.generateArray();
+    }
+    res.render("account-change-info", {
+        title: "Thay đổi thông tin tài khoản",
+        user: req.user,
+        cartProduct: cartProduct
+    });
 };
 
 exports.postAccountChange = (req, res, next) => {
-  req.user.firstName = req.body.firstName;
-  req.user.lastName = req.body.lastName;
-  req.user.email = req.body.email;
-  req.user.address = req.body.address;
-  req.user.phoneNumber = req.body.phoneNumber;
-  req.user.save();
-  res.redirect("/account");
+    req.user.firstName = req.body.firstName;
+    req.user.lastName = req.body.lastName;
+    req.user.email = req.body.email;
+    req.user.address = req.body.address;
+    req.user.phoneNumber = req.body.phoneNumber;
+    req.user.save();
+    res.redirect("/account");
 };
